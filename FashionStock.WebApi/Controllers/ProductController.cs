69ccec601a590e514b88ae2a533f6d2efe3bfc97
2004 <jaime.24.24.2004@gmail.com>
2025-01-30 20:11:19 +0000

    Criação da WebApi e de um endpoint para mostrar os produtos na database

---
+++ FashionStock.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using Azure.Core;
+using FashionStock.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FashionStock.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly BusinessContext _businessContext;
+
+        public ProductController(BusinessContext businessContext)
+        {
+            _businessContext = businessContext;
+        }
+
+        [HttpGet("/getproducts")]
+        public async Task<IActionResult> GetProducts()
+        {
+            var productTable = await _businessContext.Products.ToListAsync();
+
+            if (!productTable.Any())
+            
+                return NotFound();
+            else
+
+            return Ok(productTable);
+        }
+    }
+}
