f5120f2f3fdd3ee9f0e97974cce862f647328aa5
BrendaGermano <brenda-souza45@hotmail.com>
2025-02-06 18:33:20 +0000

    Revert "Revert 7 feature/bugflix"

--- FashionStock/Entities/Product.cs
+++ FashionStock/Entities/Product.cs
@@ -15,6 +15,5 @@ namespace FashionStock.Entities
         public int Quantity { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
-
     }
 }
